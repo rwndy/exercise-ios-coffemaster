d3c1ef36afc026dfb20ccdf8299042fdeb80e2ee
Riwandi <riwandi.zamani@mncgroup.com>
2023-12-05 16:00:21 +0700

    Initial Commit

---
+++ CoffeMaster/CoffeMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeMasterApp.swift
+//  CoffeMaster
+//
+//  Created by Riwandi on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoffeMaster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoffeMaster
+//
+//  Created by Riwandi on 05/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
